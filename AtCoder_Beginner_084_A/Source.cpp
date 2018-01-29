b488ccea32eaba95d227babe2ac9fb8c959b89cc
りーま <stoneriver213@outlook.jp>
2018-01-29 23:04:13 +0900

    プロジェクト ファイルを追加します。

---
+++ AtCoder_Beginner_084_A/Source.cpp
@@ -0,0 +1,15 @@
+/*
+–â‘è•¶
+12 ŒŽ 30 “ú‚Ì M Žž‚©‚çŽŸ‚Ì”N‚É‚È‚é‚Ü‚Å‚Í‰½ŽžŠÔ‚©A‹‚ß‚Ä‚­‚¾‚³‚¢B
+*/
+
+#include<iostream>
+
+int main() {
+	int m;
+	std::cin >> m;
+
+	std::cout << 48 - m << std::endl;
+
+	return 0;
+}
